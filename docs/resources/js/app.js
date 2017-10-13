e1a73240960fe147bc48b0d7f6fac9337133b128
omidmanai <gusmanom@student.gu.se>
2017-10-13 17:18:48 +0200

    JSON parsing added based on json_test file

---
+++ docs/resources/js/app.js
@@ -0,0 +1,10 @@
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var obj = JSON.parse(this.responseText);
+        console.log(obj.processes[1].class + " " + obj.processes[1].name);
+    }
+};
+xmlhttp.open("GET", "json_test.txt", true);
+xmlhttp.send();
