bfec433b9e731176dbd45b9753e5cdaa78f494bf
omidmanai <gusmanom@student.gu.se>
2017-10-15 18:34:40 +0200

    Changed json parsing method from pure JS to jQuery

--- docs/resources/js/app.js
+++ docs/resources/js/app.js
@@ -1,10 +1,14 @@
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var obj = JSON.parse(this.responseText);
-        console.log(obj.processes[1].class + " " + obj.processes[1].name);
-    }
-};
-xmlhttp.open("GET", "json_test.txt", true);
-xmlhttp.send();
+// var xmlhttp = new XMLHttpRequest();
+// xmlhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         var obj = JSON.parse(this.responseText);
+//         console.log(obj.processes[1].class + " " + obj.processes[1].name);
+//     }
+// };
+// xmlhttp.open("GET", "json_test.txt", true);
+// xmlhttp.send();
+
+$.getJSON("json_test.txt", function(json) {
+    console.log(json);
+});
