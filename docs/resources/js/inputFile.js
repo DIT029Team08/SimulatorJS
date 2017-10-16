81add2304990124b8a2f652029139d3620b26c28
firsou <guschefi@student.gu.se>
2017-10-16 22:16:37 +0200

    Changed wrong file type error message

--- docs/resources/js/inputFile.js
+++ docs/resources/js/inputFile.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#JSONFile").on('click change', function validateJsonFile(){
         var file = document.querySelector("#JSONFile");
         if(/\.json$/i.test(file.files[0].name) === false){
-            $("#JSONFile").get(0).setCustomValidity("enter correct format");
+            $("#JSONFile").get(0).setCustomValidity("Please select a .JSON File");
         }
         else {
             $("#JSONFile").get(0).setCustomValidity('');
