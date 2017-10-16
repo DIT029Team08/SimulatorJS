c3ccd47f654224c2ea03bc7ac010f850e598b5a1
gussinku <gussinku@student.gu.se>
2017-10-16 17:16:20 +0200

    input file can now only send .json format and error message display successfully

--- docs/inputFile.js
+++ docs/inputFile.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
     $("#submitButtonId").attr("disabled", "disabled");
     $("#JSONFile").on('click change', function validateJsonFile(){
         var file = document.querySelector("#JSONFile");
-        if(/\.JSON$/i.test(file.files[0].name) === false){
-            $('#JSONFile').get(0).setCustomValidity('enter correct format');
+        if(/\.json$/i.test(file.files[0].name) === false){
+            $("#JSONFile").get(0).setCustomValidity("enter correct format");
+        }
+        else {
+            $("#JSONFile").get(0).setCustomValidity('');
         }
         $("#submitButtonId").removeAttr("disabled");
     });
