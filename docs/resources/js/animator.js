9244f851d59dc59bea1e2a4d8f15a43984bc0d0b
omidmanai <gusmanom@student.gu.se>
2017-10-25 16:31:54 +0200

    animator.js now returns the processes in the json file and their names

--- docs/resources/js/animator.js
+++ docs/resources/js/animator.js
@@ -1,26 +1,28 @@
 $.getJSON("../js/json_test.JSON", function(animator){
 
-
-
     var mainDiv = document.getElementById("outputJSON");
     const processDivClassName = "processes";
-    const betweenDivClassname = "betweenProcess";
+    const betweenDivClassname = "betweenProcesses";
+    const lifelines = "lifeLine";
 
     for(var i = 0; i < animator.processes.length; i++) {
 
         var div = document.createElement("div");
         div.className = processDivClassName;
         div.innerHTML =
-            animator.processes[i].class.toString() + " " +
-            animator.processes[i].name.toString();
+            animator.processes[i].name.toString() + ": " +
+            animator.processes[i].class.toString();
         mainDiv.appendChild(div);
-        console.log(div);
+
+        var lifeLineDiv = document.createElement("div");
+        lifeLineDiv.className = lifelines;
+        div.appendChild(lifeLineDiv);
 
         if (i < animator.processes.length) {
             var emptyDiv = document.createElement("div");
-            div.className = betweenDivClassname;
+            emptyDiv.className = betweenDivClassname;
 
             mainDiv.appendChild(emptyDiv);
-    }
+        }
     }
 });
