59685229af22b1be0196c54ad518fa3846351c71
Noahnom <gusfawom@student.gu.se>
2017-10-28 12:01:26 +0200

    added file reading code

--- docs/resources/js/inputFile.js
+++ docs/resources/js/inputFile.js
@@ -1,4 +1,14 @@
 window.onload = function() {
     var fileInput = document.getElementById('JSONFile');
+
+    fileInput.addEventListener('change', function(e) {
+        var fullFile = fileInput.files[0];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            localStorage.setItem('stringJSON', reader.result);
+        };
+        reader.readAsText(fullFile);
+    });
 }
 
