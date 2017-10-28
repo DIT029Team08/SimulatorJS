16e37b92d489e76fb546fc89b57567070ba0f612
Noahnom <gusfawom@student.gu.se>
2017-10-28 11:57:19 +0200

    input file new function,mentioned inputFile in animationPage

--- docs/resources/js/inputFile.js
+++ docs/resources/js/inputFile.js
@@ -1,13 +1,4 @@
-$(document).ready(function(){
-    $("#JSONFile").on('click change', function validateJsonFile(){
-        var file = document.querySelector("#JSONFile");
-        if(/\.json$/i.test(file.files[0].name) === false){
-            $("#JSONFile").get(0).setCustomValidity("Please select a .JSON File");
-        }
-        else {
-            $("#JSONFile").get(0).setCustomValidity('');
-        }
-    
-    });
-});
+window.onload = function() {
+    var fileInput = document.getElementById('JSONFile');
+}
 
