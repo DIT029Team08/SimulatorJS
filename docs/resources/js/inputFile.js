73f86c1341338ae0f02907ba6278cb0e44838066
Noahnom <gusfawom@student.gu.se>
2017-10-28 12:04:23 +0200

    added fileInput check

--- docs/resources/js/inputFile.js
+++ docs/resources/js/inputFile.js
@@ -1,19 +1,21 @@
 window.onload = function() {
     var fileInput = document.getElementById('JSONFile');
+    
+    if (fileInput) {
+        fileInput.addEventListener('change', function(e) {
+            var fullFile = fileInput.files[0];
+            var fileValue = fileForm.file.value;
+            var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
 
-    fileInput.addEventListener('change', function(e) {
-        var fullFile = fileInput.files[0];
-        var fileValue = fileForm.file.value;
-        var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
+            
+            if (fileValue.match(regTypesAllowed)) {
+                var reader = new FileReader();
 
-        
-        if (fileValue.match(regTypesAllowed)) {
-            var reader = new FileReader();
-
-            reader.onload = function(e) {
-                localStorage.setItem('stringJSON', reader.result);
-            };
-            reader.readAsText(fullFile);
-        }
-    });
+                reader.onload = function(e) {
+                    localStorage.setItem('stringJSON', reader.result);
+                };
+                reader.readAsText(fullFile);
+            }
+        });
+    }
 };
