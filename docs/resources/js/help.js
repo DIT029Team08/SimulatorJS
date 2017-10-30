c82cb44eec28d0a2947ddfaba9b9e3903f6e4bd4
wrang308 <gusmarwr@student.gu.se>
2017-10-30 13:38:16 +0100

    added help window

---
+++ docs/resources/js/help.js
@@ -0,0 +1,9 @@
+
+function newHelpWindowtst(){
+
+	var windowObjectReference;
+	var strWindowFeatures = "left=200, top=100, width=800, height=500, menubar=no, resizable=no, status=no";
+
+	windowObjectReference = window.open("../pages/help.html", "_blank", strWindowFeatures);
+
+}
