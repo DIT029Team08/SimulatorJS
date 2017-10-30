de7614edc4d618e793c9fc4ee55f88d8da9a1608
omidmanai <gusmanom@student.gu.se>
2017-10-30 14:27:35 +0100

    Arrows have their messages now

--- docs/resources/js/animator.js
+++ docs/resources/js/animator.js
@@ -6,6 +6,7 @@ window.onload = function animateJSON() {
     const processDivClassName = "processes";
     const lifelines = "lifeLine";
     const arrowDivClassName = "arrows";
+    const messageDivClassName = "messages";
 
     for(var i = 0; i < animator.processes.length; i++) {
 
@@ -32,34 +33,43 @@ window.onload = function animateJSON() {
         var endPosition = getPosition(document.querySelector("#" + animator.diagram.content[0].content[i].to.toString()));
 
         var arrow = document.createElement("div");
+        arrow.className = arrowDivClassName;
         var svg = document.createElementNS('http://www.w3.org/2000/svg',"svg");
         var polygon = document.createElementNS('http://www.w3.org/2000/svg','polygon');
-        svg.setAttribute("preserveAspectRatio", "xMaxYMid slice");
+        var message = document.createElement("div");
+        message.className = messageDivClassName;
 
+
+        svg.setAttribute("preserveAspectRatio", "xMaxYMid slice");
         svg.setAttribute("viewBox","0 0 1400 14");
+
         if(startPosition.x > endPosition.x){
             // arrow.setAttribute("width", (startPosition.x - endPosition.x) + "px");
-            svg.setAttribute("width", (startPosition.x - endPosition.x) + "px");
+            var arrowLength = startPosition.x - endPosition.x;
+            svg.setAttribute("width", arrowLength  + "px");
             arrow.style.transform = "rotate(180deg)";
             arrow.style.left = startPosition.x - 30 + 'px';
+            message.style.left = arrowLength/2 + 'px';
+            message.style.transform = "rotate(180deg)";
         }
         else{
             // arrow.setAttribute("width", (endPosition.x - startPosition.x) + "px");
+            var arrowLength = startPosition.x - endPosition.x;
             svg.setAttribute("width", (endPosition.x - startPosition.x) + "px");
             arrow.style.left = startPosition.x - 30 + 'px';
+            message.style.right = arrowLength/2 + 'px';
         }
 
         svg.setAttribute("height","14");
         polygon.setAttribute("points", "1400,7 1385,1 1390,6 0,6 0,8 1390,8 1385,13 1400,7");
-        arrow.className = arrowDivClassName;
 
+
+        message.innerHTML =
+            animator.diagram.content[0].content[i].message.toString();
 
 
         arrow.style.top = startPosition.y + i*a - 20 + 'px';
         arrow.style.right =  ((startPosition.x) - (startPosition.x - endPosition.x)) + 'px';
-
-        // arrow.innerHTML =
-        //     animator.diagram.content[0].content[i].message.toString();
 
         // arrow.style.position = "absolute";
         // arrow.style.left = document.getElementById(animator.diagram.content[0].content[i].from.toString()).getBoundingClientRect().left.toString() + 'px';
@@ -69,6 +79,7 @@ window.onload = function animateJSON() {
 
 
         // arrow.innerHTML = animator.diagram.content[0].content[i].message.toString();
+        arrow.appendChild(message);
         svg.appendChild(polygon);
         arrow.appendChild(svg);
         mainDiv.appendChild(arrow);
