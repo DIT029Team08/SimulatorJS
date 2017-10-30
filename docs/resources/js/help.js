cf9bf3271f2a27ae2ad2b8650c7c925bd076bc8c
David Larsson <guslardaac@student.gu.se>
2017-10-30 15:11:41 +0100

    Removed lorem ipsum, and changed the size of the new window

--- docs/resources/js/help.js
+++ docs/resources/js/help.js
@@ -2,8 +2,7 @@
 function newHelpWindowtst(){
 
 	var windowObjectReference;
-	var strWindowFeatures = "left=200, top=100, width=800, height=500, menubar=no, resizable=no, status=no";
+	var strWindowFeatures = "left=200, top=100, width=350, height=500, menubar=no, resizable=no, status=no";
 
 	windowObjectReference = window.open("../pages/help.html", "_blank", strWindowFeatures);
-
 }
