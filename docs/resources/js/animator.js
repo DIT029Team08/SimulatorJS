eb28831b98f608cc44c75871bb752ee99b7fe695
gussinku <gussinku@student.gu.se>
2017-11-08 08:52:03 +0100

    added logic for smooth scroll of main div containing id outputJson and function remove item so that jsonfile is not stored in browser evertime we load

--- docs/resources/js/animator.js
+++ docs/resources/js/animator.js
@@ -12,7 +12,13 @@ const messageDivClassName = "messages";
 
 var animator = JSON.parse(localStorage.getItem('stringJSON'));
 var mainDiv = document.getElementById("outputJSON");
-
+// Get a reference to the div you want to auto-scroll.
+var mainDivElement = document.querySelector("outputJSON");
+// Create an observer and pass it a callback.
+var observer = new MutationObserver(scrollToBottom);
+// Tell it to look for new children that will change the height.
+var config = {childList: true};
+observer.observe(mainDivElement, config);
 
 window.onload = function animateJSON() {
 
@@ -291,3 +297,38 @@ function createLifeline(animator, i) {
     div.appendChild(lifeLineDiv);
 
 }
+
+// First, define a helper function.
+function animateScroll(duration) {
+    var start = mainDivElement.scrollTop;
+    var end = mainDivElement.scrollHeight;
+    var change = end - start;
+    var increment = 20;
+    function easeInOut(currentTime, start, change, duration) {
+      // by Robert Penner  Easing Functions
+      currentTime /= duration / 2;
+      if (currentTime < 1) {
+        return change / 2 * currentTime * currentTime + start;
+      }
+      currentTime -= 1;
+      return -change / 2 * (currentTime * (currentTime - 2) - 1) + start;
+    }
+
+    function animate(elapsedTime) {
+      elapsedTime += increment;
+      var position = easeInOut(elapsedTime, start, change, duration);
+      mainDivElement.scrollTop = position;
+      if (elapsedTime < duration) {
+        setTimeout(function() {
+          animate(elapsedTime);
+        }, increment)
+      }
+    }
+    animate(0);
+  }
+  
+  // Here's our main callback function we passed to the observer
+  function scrollToBottom() {
+    var duration = 1000 // Or however many milliseconds you want to scroll to last
+    animateScroll(duration);
+  }
--- docs/resources/js/inputFile.js
+++ docs/resources/js/inputFile.js
@@ -8,14 +8,22 @@ window.onload = function() {
             var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
 
             
-            if (fileValue.match(regTypesAllowed)) {
+            if (fileValue.match(regTypesAllowed) && 'localStorage' in $window && $window.localStorage !== null) {
                 var reader = new FileReader();
-
+                try{
                 reader.onload = function(e) {
-                    localStorage.setItem('stringJSON', reader.result);
+                    $window.localStorage.setItem('stringJSON', reader.result);
+                    $window.localStorage.removeItem('stringJSON');
                 };
                 reader.readAsText(fullFile);
+            }catch(e){
+                reader = false;
+            }  
             }
         });
     }
 };
+
+
+    /**JavaScript
+    var scroll = setInterval(function(){ window.scrollBy(0,1000); }, 2000); */
