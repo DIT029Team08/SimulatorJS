b3daeaa0fa17e4a71f14c30dcae7ac4d7fe46d97
Noahnom <gusfawom@student.gu.se>
2017-11-25 23:26:17 +0100

    added input.js documentation

--- docs/resources/js/inputFile.js
+++ docs/resources/js/inputFile.js
@@ -4,14 +4,14 @@ window.onload = function() {
     if (fileInput) {
         fileInput.addEventListener('change', function(e) {
             var fullFile = fileInput.files[0];
-            var fileValue = fileForm.file.value;
+            var fileValue = fileForm.file.value; // Retrieves file extension submitted
             var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
 
             
             if (fileValue.match(regTypesAllowed)) {
                 var reader = new FileReader();
                 reader.onload = function(e) {
-                    localStorage.setItem('stringJSON', reader.result);
+                    localStorage.setItem('stringJSON', reader.result); // Once file is read, JSON text is put in local storage
                 };
                 reader.readAsText(fullFile);
             }
