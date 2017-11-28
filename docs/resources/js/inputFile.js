4065111ba3ffeacd78e3eb47a8b7594730820b32
gussinku <gussinku@student.gu.se>
2017-11-28 14:29:00 +0100

    added list view of multiple json uploads

--- docs/resources/js/inputFile.js
+++ docs/resources/js/inputFile.js
@@ -1,37 +1,51 @@
 window.onload = function() {
-    var fileInput = document.getElementById('JSONFile');
+	var fileInput = document.getElementById("JSONFile");
     
-    if (fileInput) {
-        fileInput.addEventListener('change', function(e) {
-            var fullFile = fileInput.files[0];
-            var fileValue = fileForm.file.value;
-            var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
+	if (fileInput) {
+		fileInput.addEventListener("change", function(e) {
+			var fullFile = fileInput.files[0];
+			var fileValue = fileForm.file.value;
+			var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
 
             
-            if (fileValue.match(regTypesAllowed)) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    localStorage.setItem('stringJSON', reader.result);
-                };
-                reader.readAsText(fullFile);
-            }
-        });
-    }
+			if (fileValue.match(regTypesAllowed)) {
+				var reader = new FileReader();
+				reader.onload = function(e) {
+					localStorage.setItem("stringJSON", reader.result);
+				};
+				reader.readAsText(fullFile);
+			}
+		});
+	}
 };
+var selDiv = "";
+document.addEventListener("DOMContentLoaded", init, false);  //event is fired when the initial HTML document has been completely loaded and parsed
 
+function init() {
+	document.querySelector("#JSONFile").addEventListener("change", handleFileSelect, false);
+	selDiv = document.querySelector("#selectedFiles");
+}
 
-    /**JavaScript
-    var scroll = setInterval(function(){ window.scrollBy(0,1000); }, 2000);
-         
-            if (fileValue.match(regTypesAllowed) && && $window.localStorage !== null) {
-                var reader = new FileReader();
-                try{
-                reader.onload = function(e) {
-                    localStorage.setItem('stringJSON', reader.result);
-                    //localStorage.removeItem('stringJSON');
-                };
-                reader.readAsText(fullFile);
-            }catch(e){
-                reader = false;
-            }  
-            } */
+function handleFileSelect(e) {
+
+	if (!e.target.files) return;
+
+	selDiv.innerHTML = ""; //sets the HTML content (inner HTML) of an element.
+
+	var files = e.target.files;
+    
+	//goes through a list of selected jsons and saves and appends to the div html
+	for (var i = 0; i < files.length ; i++) {
+		var selectedJson;
+		if (this.files.length < 4){
+			selectedJson = files[i];
+		}
+		else {
+			document.getElementById("JSONFile").setCustomValidity("Max 3 json per upload");
+			document.getElementById("JSONFile").value = null; //..clears the input file
+			
+		}
+		selDiv.innerHTML += selectedJson.name + "<br/>"; //returns the HTML content (inner HTML) of an element.
+
+	}
+}
