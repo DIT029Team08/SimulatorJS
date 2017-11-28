9487da7224c472407805d4274186eb5b3b239f72
David Larsson <guslardaac@student.gu.se>
2017-11-28 15:08:59 +0100

    Added the error message if the file selected isn't a JSON. Also made the button unclickable in that case.

--- public/js/inputFile.js
+++ public/js/inputFile.js
@@ -1,19 +1,29 @@
 window.onload = function() {
     var fileInput = document.getElementById('JSONFile');
-    
+    var button = document.getElementById('submitbutton');
+    var message = document.getElementById('error')
+    message.style.opacity = 0;
+
     if (fileInput) {
         fileInput.addEventListener('change', function(e) {
             var fullFile = fileInput.files[0];
             var fileValue = fileForm.file.value; // Retrieves file extension submitted
             var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
 
-            
             if (fileValue.match(regTypesAllowed)) {
+                console.log("WORKS")
+                button.disabled = false;
+                message.style.opacity = 0;
                 var reader = new FileReader();
                 reader.onload = function(e) {
                     localStorage.setItem('stringJSON', reader.result); // Once file is read, JSON text is put in local storage
                 };
                 reader.readAsText(fullFile);
+            }
+            else{
+                console.log("NOPE")
+                button.disabled = true;
+                message.style.opacity = 100;
             }
         });
     }
