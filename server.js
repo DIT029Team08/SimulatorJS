c954d94722b5ade83d29ff093b10fef8734846b5
omidmanai <gusmanom@student.gu.se>
2017-11-29 17:23:56 +0100

    commented some lines in server

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ connections = [];
 server.listen(process.env.PORT || 3000);
 console.log('Server running ...');
 
-// app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
 
@@ -28,21 +27,13 @@ app.get('/', function(req, res) {
     }
 });
 
-// app.use('/docs/resources/styles', express.static(path.join(__dirname, '/docs/resources/styles')));
-// app.use("/docs/resources/styles", express.static( __dirname + '/docs/resources/styles'));
 
 io.sockets.on('connection', function(socket) {
     connections.push(socket);
 
-
-    // socket.id = Math.floor(Date.now() * Math.random());
-    // socket.on('data', function(data) {
-    //     socket.write('ID: ' + socket.id);
-    // });
     console.log('Socket ID is: %s', socket.id);
 
     console.log('One Connected: %s sockets connected', connections.length);
-    // io.to(connections[0]).emit('send message', {msg: 'Hello'});
 
     //Disconnect
     socket.on('disconnect', function(socket) {
@@ -62,7 +53,8 @@ io.sockets.on('connection', function(socket) {
     socket.on('begin animation', function (data) {
         console.log("Users after animation started " + connections.length);
         io.sockets.emit('begin animation', data.animator);
-        io.to(connections[0].id).emit('send message', data.animator.diagram.content[0].content[0].from.toString());
+        // io.to(connections[0].id).emit('send message', data.animator.diagram.content[0].content[0].from.toString());
+        // io.to(connections[0].id).emit('send message', data.animator.type.toString());
     })
 
 
