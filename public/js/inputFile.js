77d7665ab88d6f022bc9335bf34b2732f1f364b8
David Larsson <guslardaac@student.gu.se>
2017-12-05 11:30:06 +0100

    Started working on inputting miltiple diagrams to be able to switch between them.

--- public/js/inputFile.js
+++ public/js/inputFile.js
@@ -2,31 +2,70 @@ var fileInput = document.getElementById('JSONFile');
 window.onload = function() {
     var button = document.getElementById('submitbutton');
     var message = document.getElementById('error');
+    var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
     message.style.opacity = 0;
 
     if (fileInput) {
         fileInput.addEventListener('change', function(e) {
-            var fullFile = fileInput.files[0];
-            var fileValue = fileForm.file.value; // Retrieves file extension submitted
-            var regTypesAllowed =  /(.*?)\.(json|JSON)$/;
+            // If there are 3 files
+            if(fileInput.files.length > 2){
+                console.log("3 files");
+                var file1 = fileInput.files[0];
+                var file2 = fileInput.files[1];
+                var file3 = fileInput.files[2];
+                // Check so the files are allowed
+                if(file1.name.match(regTypesAllowed) && file2.name.match(regTypesAllowed) && file3.name.match(regTypesAllowed)){
+                    button.disabled = false;
+                    message.style.opacity = 0;
 
-            if (fileValue.match(regTypesAllowed)) {
-                button.disabled = false;
-                message.style.opacity = 0;
-                var reader = new FileReader();
-                reader.onload = function(e) {
+                }
+                // If not allowed
+                else{
+                    button.disabled = true;
+                    message.style.opacity = 100;
+                }
+            }
+
+            // If there are 2 files
+            else if(fileInput.files.length > 1){
+                console.log("2 files");
+                var file1 = fileInput.files[0];
+                var file2 = fileInput.files[1];
+                // Check so the files are allowed
+                if(file1.name.match(regTypesAllowed) && file2.name.match(regTypesAllowed)){
+                    button.disabled = false;
+                    message.style.opacity = 0;
+
+                }
+                // If not allowed
+                else{
+                    button.disabled = true;
+                    message.style.opacity = 100;
+                }
+            }
+
+            // If there is only 1 file
+            else{
+                console.log("1 file");
+                var file1 = fileInput.files[0];
+                // Check so the file is allowed
+                if (file1.name.match(regTypesAllowed)){
+                    button.disabled = false;
+                    message.style.opacity = 0;
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
                 	localStorage.clear();
                     localStorage.setItem('stringJSON', reader.result); // Once file is read, JSON text is put in local storage
-                };
-                reader.readAsText(fullFile);
-            }
-            else{
-                button.disabled = true;
-                message.style.opacity = 100;
+                    };
+                    reader.readAsText(file1);
+                }
+                else{
+                    button.disabled = true;
+                    message.style.opacity = 100;
+                }
             }
         });
     }
-
 };
 var selDiv = "";
 document.addEventListener("DOMContentLoaded", init, false);  //event is fired when the initial HTML document has been completely loaded and parsed
@@ -35,7 +74,10 @@ function init() {
 	fileInput.addEventListener("change", handleFileSelect, false);
 	selDiv = document.querySelector("#selectedFiles");
 }
-
+// Helper function to get extension.
+function getFileExtension(filename) {
+    return filename.name.split('.').pop();
+}
 function handleFileSelect(e) {
 
 	if (!e.target.files) return;
