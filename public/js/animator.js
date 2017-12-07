35af28ca7d432598cf122930331e5cb019f4fd9d
wrang308 <gusmarwr@student.gu.se>
2017-12-07 22:36:30 +0100

    fixed the reset button so it only shows the messages once even if clicked multiple times.

--- public/js/animator.js
+++ public/js/animator.js
@@ -38,10 +38,11 @@ function outputAnimation(animator, tmpSocketIds) {
         log.innerHTML = "";
         counter = 0;
         logCounter = 0;
-
+        objectIndex = 0;
 
         if (animator.type === 'sequence_diagram') {
                 scrollBoolean = true;
+
                 llHeight = 150;
                 //Selects the processes array in JSON File and iterates for every element
                 processDiv = document.createElement("div");
@@ -81,7 +82,10 @@ function outputAnimation(animator, tmpSocketIds) {
             addColor = false;
             arrowCounter = 0;
             createNodes(animator.diagram, mainDiv);
-            scrollBoolean = false;
+            setTimeout(function () {
+                scrollBoolean = false;
+                console.log(scrollBoolean);
+            }, arrowCounter * 1000);
             //createLog(animator, 0, 0, 0);
         }
 // Checks if it's a class diagram
@@ -816,6 +820,7 @@ function dragElement(element) {
 
 
 function createNodes(object, frameToAppend){
+    console.log("Wats upp boolean"+scrollBoolean);
     var arrayOfNodes = []; //this array will contain all the JSON Objects (nodes) inside object
     var arrayOfObjects = []; //this array will contain the "content" objects in the object
 
@@ -881,7 +886,6 @@ function createNodes(object, frameToAppend){
                 var lifelineElement = document.getElementById(object.from);
                 objectArray.push({fromNode: fromNode, toNode: toNode, messageToSend: messageToSend,
                     frameToAppend: frameToAppend, objectFrom: object.from, lifelineElement: lifelineElement});
-
                 setTimeout(function () {serverRequest(); concatLog(object.from, object.to, messageToSend);}, arrowCounter * 1000);
                 arrowCounter++;
 
